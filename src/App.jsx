d2bb772aad8cce09d4a1af3dc8e61c60f3cb218f
narendravs <narendravs_mca@yahoo.com>
2025-04-11 12:20:06 +0530

    files configured to the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import MainApp from "./MainApp";
+import useDarkMode from "use-dark-mode";
+
+function App() {
+  return (
+    <div>
+      <MainApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/AppContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const AppContex = createContext();
+
+export default AppContex;
